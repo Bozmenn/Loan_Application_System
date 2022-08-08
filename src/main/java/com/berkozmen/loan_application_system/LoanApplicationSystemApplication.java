35257a3133685bd5f5537425cbeb1eb11d46605e
Berk Özmen <99732114+Bozmenn@users.noreply.github.com>
2022-08-08 09:48:47 +0300

    README.md added

--- src/main/java/com/berkozmen/loan_application_system/LoanApplicationSystemApplication.java
+++ src/main/java/com/berkozmen/loan_application_system/LoanApplicationSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoanApplicationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanApplicationSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationSystemApplication.class, args);
+    }
 
 }
